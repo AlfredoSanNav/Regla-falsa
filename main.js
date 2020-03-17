913065fa974ff2f1aeeb4625f6c6d0796ceb00d5
Alfredo Sánchez Nava <asanchez47@ucol.mx>
2020-03-16 21:42:12 -0600

    Agrega el porcentaje

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ var xu = Number(document.querySelector("#xu").value);
 var fxu = Number(document.querySelector("#fxu").value);
 var btn1 = document.querySelector('#btn1');
 function formula(x){ return((Math.pow(x,3))+(Math.pow(x,2))-3*(x)-3) }
+function porcentaje(xrv, xr){ return (Math.abs(((xr-xrv)/xr)*100)) }
+function formula2(x){ return (Math.atan(x)+x-1) }
 
 btn1.addEventListener('click', () => {
     let xl = Number(document.querySelector("#xl").value) ;
@@ -13,13 +15,18 @@ btn1.addEventListener('click', () => {
     let i = 1
     let fxr
     let xr
+    let xrv = xr
+    let por 
 while(i < 15){
+    xrv = xr
     xr = xu - ((fxu * (xl-xu))/(fxl - fxu));
-    fxr = formula(xr).toFixed(7)
+    fxr = formula2(xr).toFixed(7)
+    por = (porcentaje(xrv, xr)).toFixed(7)
     
     console.log("Iteración " + i)
     console.log("Xr = " + xr)
     console.log("f(xr) = " + fxr)
+    console.log("Porcentaje de error = " + por)
     i++
 
     if(Math.sign(fxr) === -1 && xr > xl){
@@ -29,8 +36,8 @@ while(i < 15){
         xu = xr
     }
     
-    fxu = formula(xu).toFixed(7)
-    fxl = formula(xl).toFixed(7)
+    fxu = formula2(xu).toFixed(7)
+    fxl = formula2(xl).toFixed(7)
 
 }
 
