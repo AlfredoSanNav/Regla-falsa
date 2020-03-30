9be05c31961de6580bfd6aa09d554bbd22a994f1
Alfredo Sánchez Nava <asanchez47@ucol.mx>
2020-03-29 22:00:08 -0600

    Agrega la estructura para las 3 ecuaciones pero todavía no funciona xc

--- main.js
+++ main.js
@@ -4,21 +4,30 @@ var btn1 = document.querySelector('#btn1');
 
 
 function porcentaje(xrv, xr){ return (((xr-xrv)/xr)*100) };
-
-// f(x)=e^(-x)-ln x 
-function formula1(x){ return(Math.pow(Math.E, -x) - Math.log(x)) };
-// x^3+x^2-3x-3 
-function formula2(x){ return((Math.pow(x,3))+(Math.pow(x,2))-3*(x)-3)}
-// f(x)=arctang(x)+x-1
-function formula3(x){ return ((Math.atan(x))+ x - 1) };
 var tas = "<p> <center>";
 
 btn1.addEventListener('click', () => {
     let xl = Number(document.querySelector("#xl").value) ;
     let xu = Number(document.querySelector("#xu").value);
+    let ecuacion = document.querySelector("#ecuacion")
 
-    let fxl = formula1(xl)
-    let fxu = formula1(xu);    
+    switch(ecuacion){
+        case "primera":
+            // f(x)=e^(-x)-ln x 
+            function ecuacionElegida(x){ return(Math.pow(Math.E, -x) - Math.log(x)) };
+            break
+        case "segunda":
+            // x^3+x^2-3x-3 
+            function ecuacionElegida(x){ return((Math.pow(x,3))+(Math.pow(x,2))-3*(x)-3)}
+            break
+        case "tercera":
+            // f(x)=arctang(x)+x-1
+            function ecuacionElegida(x){ return ((Math.atan(x))+ x - 1) };
+            break
+        }
+
+    let fxl = ecuacionElegida(xl)
+    let fxu = ecuacionElegida(xu)    
     let por = 100
     let xrv = (xu - ((fxu * (xl-xu))/(fxl - fxu))).toFixed(7);
     let i = 0
@@ -27,7 +36,7 @@ btn1.addEventListener('click', () => {
     while (i < 30){
     
     let xr = xu - ((fxu * (xl-xu))/(fxl - fxu)).toFixed(7);
-    let fxr = formula1(xr).toFixed(7);
+    let fxr = ecuacionElegida(xr).toFixed(7);
     let por = porcentaje(xr, xrv).toFixed(7);
 
     console.log(`Iteración ${i}:  xr = ${xr}  f(xr) = ${fxr}  por = ${por}` )
@@ -42,8 +51,8 @@ btn1.addEventListener('click', () => {
 
     xrv = xr;
     i++
-    fxu = formula1(xu).toFixed(7);
-    fxl = formula1(xl).toFixed(7);
+    fxu = ecuacionElegida(xu).toFixed(7);
+    fxl = ecuacionElegida(xl).toFixed(7);
     
 }
 
