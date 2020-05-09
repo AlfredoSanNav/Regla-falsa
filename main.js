3456c676e85378bf146eadea99f7cb42df67a8d6
PaAdRaAg <54459507+PaAdRaAg@users.noreply.github.com>
2020-05-09 12:32:06 -0500

    si se puede :v

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
-var xl = Number(document.querySelector("#xl").value) ;
-var xu = Number(document.querySelector("#xu").value);
 var ecuacion = document.querySelector("#ecuacion")
 var btn1 = document.querySelector('#btn1');
  
 
 btn1.addEventListener("click",()=>{
+    let xl = Number(document.querySelector("#xl").value);
+    let xu = Number(document.querySelector("#xu").value);
+    let fxu = Number(Math.exp(-xu)-(Math.log(xu)));
+    let fxl = Number(Math.exp(-xl)-(Math.log(xl)));
 
-    let xr = xu - ((fxu * (xl-xu))/(fxl - fxu)).toFixed(7)
+    console.log(fxu)
+    console.log(fxl)
 
+    let xr = xu-((fxu*(xl-xu))/(fxl-fxu));
+    console.log(xr)
+
+    for(i=0; i<10; i++){
+        
+    }
 })
