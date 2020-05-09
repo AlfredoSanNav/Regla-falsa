c5f785fd66753aca2acf99b766ae85559277271d
Alfredo Sánchez Nava <asanchez47@ucol.mx>
2020-05-09 10:41:24 -0500

    Agrega el if

--- main.js
+++ main.js
@@ -2,7 +2,23 @@ var xl = Number(document.querySelector("#xl").value) ;
 var xu = Number(document.querySelector("#xu").value);
 var ecuacion = document.querySelector("#ecuacion")
 var btn1 = document.querySelector('#btn1');
+var ejemplo 
 
-btn1.addEventListener(click,()=>{
-    
+btn1.addEventListener("click",()=>{
+    if(ecuacion == "primera"){
+
+        function ecuacionElegida(x){}
+        ejemplo = 1
+
+    }else if(ecuacion == "segunda"){
+
+        function ecuacionElegida(x){}
+        ejemplo = 2
+
+    }else if (ecuacion == "tercera"){
+
+        function ecuacionElegida(x){}
+        ejemplo = 3
+    }
+    console.log(ejemplo)
 })
