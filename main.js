c2466f3b1d4030a450350e934f6dfcd58cfcc18c
Alfredo Sánchez Nava <asanchez47@ucol.mx>
2020-05-09 11:10:51 -0500

    Ayuda diosito

--- main.js
+++ main.js
@@ -2,24 +2,15 @@ var xl = Number(document.querySelector("#xl").value) ;
 var xu = Number(document.querySelector("#xu").value);
 var ecuacion = document.querySelector("#ecuacion")
 var btn1 = document.querySelector('#btn1');
-var ejemplo 
+ 
 
 btn1.addEventListener("click",()=>{
+    let fxu = (Math.pow(Math.E, -xu) - Math.log(xu))
+    let fxl = (Math.pow(Math.E, -xl) - Math.log(xl))
+    let xr = xu - ((fxu * (xl-xu))/(fxl - fxu)).toFixed(7)
+    let fxr = (Math.pow(Math.E, -xr) - Math.log(xr))
 
-    if(ecuacion == "primera"){
+    console.log(fxu + fxl + xr + fxr)
 
-        function ecuacion(x){}
-        ejemplo = 1
 
-    }else if(ecuacion == "segunda"){
-
-        function ecuacion(x){}
-        ejemplo = 2
-
-    }else if (ecuacion == "tercera"){
-
-        function ecuacion(x){}
-        ejemplo = 3
-    }
-    console.log(ejemplo)
 })
