d911cadb57509485743b85b52d75d73939b21ddc
PaAdRaAg <54459507+PaAdRaAg@users.noreply.github.com>
2020-05-09 11:20:20 -0500

    masa

--- main.js
+++ main.js
@@ -5,12 +5,7 @@ var btn1 = document.querySelector('#btn1');
  
 
 btn1.addEventListener("click",()=>{
-    let fxu = (Math.pow(Math.E, -xu) - Math.log(xu))
-    let fxl = (Math.pow(Math.E, -xl) - Math.log(xl))
+
     let xr = xu - ((fxu * (xl-xu))/(fxl - fxu)).toFixed(7)
-    let fxr = (Math.pow(Math.E, -xr) - Math.log(xr))
-
-    console.log(fxu + fxl + xr + fxr)
-
 
 })
