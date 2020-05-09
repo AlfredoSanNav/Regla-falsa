b150370e96f3bbf9fb38c292d1a9ceebcbd95024
Alfredo Sánchez Nava <asanchez47@ucol.mx>
2020-05-09 10:18:23 -0500

    Agrega la variable y el boton

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 var xl = Number(document.querySelector("#xl").value) ;
 var xu = Number(document.querySelector("#xu").value);
+var ecuacion = document.querySelector("#ecuacion")
 var btn1 = document.querySelector('#btn1');
+
+btn1.addEventListener(click,()=>{
+    
+})
