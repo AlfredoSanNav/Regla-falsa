5c094e967a5228b6fbed94e6a01fd2e453f6c05d
PaAdRaAg <54459507+PaAdRaAg@users.noreply.github.com>
2020-05-09 15:09:27 -0500

    si se pudo... si se pudo....

--- main.js
+++ main.js
@@ -5,33 +5,35 @@ var btn1 = document.querySelector('#btn1');
 btn1.addEventListener("click",()=>{
     let xl = Number(document.querySelector("#xl").value);
     let xu = Number(document.querySelector("#xu").value);
-    let fxu = Number(Math.exp(-xu)-(Math.log(xu)));
-    let fxl = Number(Math.exp(-xl)-(Math.log(xl)));
+    let fxu = Number(Math.exp(-xu)-(Math.log(xu))).toFixed(7);
+    let fxl = Number(Math.exp(-xl)-(Math.log(xl))).toFixed(7);
 
-    console.log(fxu)
+    console.log(xl)
+    console.log(xu)
     console.log(fxl)
+    console.log(fxu)
 
-    let xr = xu-((fxu*(xl-xu))/(fxl-fxu));
-    let fxr = Number(Math.exp(-xr)-(Math.log(xr)));
+    let xr = xu-((fxu*(xl-xu))/(fxl-fxu)).toFixed(7);
+    let fxr = Number(Math.exp(-xr)-(Math.log(xr))).toFixed(7);
 
+    console.log(xr)
     console.log(fxr)
 
     for(i=0; i<10; i++){
-        if(fxr > fxl ){
+        if(xl > xr ){
+
             xl = xr
             fxl = fxr
         }
-        if(fxr < fxu ){
+        if(xu > xr ){
+
             xu = xr
             fxu = fxr
         }
-        fxu = Number(Math.exp(-xu)-(Math.log(xu))).toFixed(7)
-        fxl = Number(Math.exp(-xl)-(Math.log(xl))).toFixed(7)
-        xr = xu-((fxu*(xl-xu))/(fxl-fxu)).toFixed(7)
-        fxr = Number(Math.exp(-xr)-(Math.log(xr))).toFixed(7)
+        fxu = Number(Math.exp(-xu)-(Math.log(xu))).toFixed(7);
+        fxl = Number(Math.exp(-xl)-(Math.log(xl))).toFixed(7);
+        xr = xu-((fxu*(xl-xu))/(fxl-fxu)).toFixed(7);
+        fxr = Number(Math.exp(-xr)-(Math.log(xr))).toFixed(7);
         console.log(` xl = ${xl} xu = ${xu} xr = ${xr} fxl = ${fxl} fxu = ${fxu} fxr = ${fxr}`)
     }
 })
-
-
-// =Number((Math.pow(xr,3))+(Math.pow(xr,2))-(3*xr)-3)
--- main3.js
+++ main3.js
@@ -5,8 +5,8 @@ var btn1 = document.querySelector('#btn1');
 btn1.addEventListener("click",()=>{
     let xl = Number(document.querySelector("#xl").value);
     let xu = Number(document.querySelector("#xu").value);
-    let fxu = Number(Math.pow(xu,3)+ (Math.pow(xu, 2)- (3*xu)-3)).toFixed(7);
-    let fxl = Number(Math.pow(xl,3)+ (Math.pow(xl, 2)- (3*xl)-3)).toFixed(7);
+    let fxu = Number((Math.atan(xu))+ xu -1).toFixed(7);
+    let fxl = Number((Math.atan(xl))+ xl -1).toFixed(7);
 
     console.log(xl)
     console.log(xu)
@@ -14,26 +14,26 @@ btn1.addEventListener("click",()=>{
     console.log(fxu)
 
     let xr = xu-((fxu*(xl-xu))/(fxl-fxu)).toFixed(7);
-    let fxr = Number(Math.pow(xr, 3)+ (Math.pow(xr, 2)- (3*xr)-3)).toFixed(7);
+    let fxr = Number((Math.atan(xr))+ xr -1).toFixed(7);
 
     console.log(xr)
     console.log(fxr)
 
     for(i=0; i<10; i++){
-        if(xl < xr ){
+        if(xl > xr ){
 
             xl = xr
             fxl = fxr
         }
-        if(xu < xr ){
+        if(xu > xr ){
 
             xu = xr
             fxu = fxr
         }
-        fxu = Number(Math.pow(xu,3)+ (Math.pow(xu, 2)- (3*xu)-3)).toFixed(7)
-        fxl = Number(Math.pow(xl,3)+ (Math.pow(xl, 2)- (3*xl)-3)).toFixed(7)
+        fxu = Number((Math.atan(xu))+ xu -1).toFixed(7)
+        fxl = Number((Math.atan(xl))+ xl -1).toFixed(7)
         xr = xu-((fxu*(xl-xu))/(fxl-fxu)).toFixed(7)
-        fxr = Number(Math.pow(xr,3)+ (Math.pow(xr, 2)- (3*xr)-3)).toFixed(7)
+        fxr = Number((Math.atan(xr))+ xr -1).toFixed(7)
         console.log(` xl = ${xl} xu = ${xu} xr = ${xr} fxl = ${fxl} fxu = ${fxu} fxr = ${fxr}`)
     }
 })
