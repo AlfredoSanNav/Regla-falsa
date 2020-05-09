4a0455515b642a7f6a2129fdf6b1b807c670520a
PaAdRaAg <54459507+PaAdRaAg@users.noreply.github.com>
2020-05-09 16:00:56 -0500

    Osiosi

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
-var ecuacion = document.querySelector("#ecuacion")
 var btn1 = document.querySelector('#btn1');
  
-
 btn1.addEventListener("click",()=>{
     let xl = Number(document.querySelector("#xl").value);
     let xu = Number(document.querySelector("#xu").value);
