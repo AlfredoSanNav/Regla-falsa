beda6df0d17378b932e3b1a24447c770b389eda7
Alfredo Sánchez Nava <asanchez47@ucol.mx>
2020-05-09 13:23:11 -0500

    Gracias diosito

--- main.js
+++ main.js
@@ -12,9 +12,23 @@ btn1.addEventListener("click",()=>{
     console.log(fxl)
 
     let xr = xu-((fxu*(xl-xu))/(fxl-fxu));
-    console.log(xr)
+    let fxr = Number(Math.exp(-xr)-(Math.log(xr)));
+
+    console.log(fxr)
 
     for(i=0; i<10; i++){
-        
+        if(fxr > fxl ){
+            xl = xr
+            fxl = fxr
+        }
+        if(fxr < fxu ){
+            xu = xr
+            fxu = fxr
+        }
+        fxu = Number(Math.exp(-xu)-(Math.log(xu))).toFixed(7)
+        fxl = Number(Math.exp(-xl)-(Math.log(xl))).toFixed(7)
+        xr = xu-((fxu*(xl-xu))/(fxl-fxu)).toFixed(7)
+        fxr = Number(Math.exp(-xr)-(Math.log(xr))).toFixed(7)
+        console.log(` xl = ${xl} xu = ${xu} xr = ${xr} fxl = ${fxl} fxu = ${fxu} fxr = ${fxr}`)
     }
 })
