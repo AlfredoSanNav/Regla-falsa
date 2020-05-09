f843b936d48d3b6d52d2fba5df1e0d3bf33ddb67
PaAdRaAg <54459507+PaAdRaAg@users.noreply.github.com>
2020-05-09 10:51:25 -0500

    sapo

--- main.js
+++ main.js
@@ -5,19 +5,20 @@ var btn1 = document.querySelector('#btn1');
 var ejemplo 
 
 btn1.addEventListener("click",()=>{
+
     if(ecuacion == "primera"){
 
-        function ecuacionElegida(x){}
+        function ecuacion(x){}
         ejemplo = 1
 
     }else if(ecuacion == "segunda"){
 
-        function ecuacionElegida(x){}
+        function ecuacion(x){}
         ejemplo = 2
 
     }else if (ecuacion == "tercera"){
 
-        function ecuacionElegida(x){}
+        function ecuacion(x){}
         ejemplo = 3
     }
     console.log(ejemplo)
